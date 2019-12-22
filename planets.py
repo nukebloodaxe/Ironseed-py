3294a17b2c6c44a5fb45956e09a484219832097a
Nuke Bloodaxe <nuke_bloodaxe@nuke-bloodaxe.org>
2019-12-22 21:29:35 +1300

    Planet rendering prep
    Preparing sections for planet rendering.  Function outlines added for next coding session.  Note:  Some of the functions seem to make very little sense, so this is taking much longer to work out than expected.

--- planets.py
+++ planets.py
@@ -17,6 +17,9 @@ These really deserve their own class and file.
 #7: Star, A:Yellow, B:Red, C:White
 
 #name, State, variation, Life/Technology level
+#Check utils2.pas, it contains most planet related details and old algos.
+
+import io, pygame, global_constants as g
 
 PlanetarySystems = {} #original code indicates these max out at 250.
 
@@ -26,7 +29,7 @@ class Planet(object):
     def __init__(self, name="Bug", state=0, grade='A', orbit = 0):
         self.name = name
         self.state = state
-        self.grade = grade #could be "mode" in original code.
+        self.grade = grade #Appears to be "mode" in original code.
         self.size = 1
         self.water = 0
         self.age = 0
@@ -41,7 +44,97 @@ class Planet(object):
         self.outpost = 0 #Extra feature, is this an outpost?
         self.owned = 0 #Extra feature, the owner of the planet.
         self.orbit = orbit
+        
+        #Planet texture related data
+        planetTexture = pygame.Surface((g.planetHeight, g.planetWidth), 0)
+        self.createPlanet()
         #Note: if you see a system with a star called 'Bug', then you have a problem.
+    
+    #Get the technology level of the planet.
+    def getTechLevel(self, system):
+        if self.orbit == 0:
+            return 0 #We are a star... although, what about Dyson spheres?
+        techLevel = -2
+        if system in ["KODUH","OLEZIAS","IYNK","TEVIX","SEKA","WIOTUN"]:
+            return 6*256 #Really...
+        
+        if system == "EXOPID":
+            
+            if 27 in g.eventFlags:
+                return 0
+            else:
+                return 6*256
+        
+        if self.state == 2:
+            if self.grade == 2:
+                techLevel == -1
+            elif self.grade == 3:
+                techLevel += self.age / 15000000
+        
+        elif self.state == 3:
+            techLevel = (self.state - 1) * 256
+            if self.grade == 1:
+                techLevel += self.age / 15000000
+            elif self.grade == 2:
+                techLevel += self.age / 1000
+            elif self.grade == 3:
+                techLevel += self.age / 800
+        
+        elif self.state == 4:
+            techLevel = (self.state + 2) * 256
+            if self.grade == 1:
+                techLevel += self.age / 400
+            elif self.grade == 2:
+                techLevel += self.age / 200
+
+        elif self.state == 5:
+            if self.grade == 1:
+                temp = self.age / 100000000
+                if temp > 9:
+                    temp = 9
+                techLevel += temp
+            elif self.grade == 2:
+                techLevel = -1
+        
+        elif self.state == 6:
+            if self.grade == 2: #Void Dwellers.
+                techLevel = 6*256
+        
+        return techLevel
+    
+    #This effectively ages the planet based on the time since last visit.
+    def adjustPlanets(self):
+        pass
+    
+    #This function handles materials and components for display.
+    def getSubQuantities(self):
+        
+        pass
+    
+    #Scans are predefined, so populate the planet item cache accordingly.
+    def getItemAmounts(self):
+        
+        pass
+    
+    #Add items to the planet, including results of surface mining/manufacturing.
+    def addItems(self):
+        
+        pass
+    
+    #Create the planet texture, which uses the random pixel height-change
+    #method to raise and lower terrain.
+    def createPlanet(self):
+        planetSurface = pygame.PixelArray(self.planetTexture)
+        
+        
+        
+        pass
+
+    #Update the planet, this is assuming there has been a change.
+    def update(self):
+        pass
+    
+    
         
 class PlanetarySystem(object):
     def __init__(self,  planets, systemName = "Buggy"):
@@ -50,6 +143,9 @@ class PlanetarySystem(object):
         self.dateMonth = 0
         self.dateYear = 0
         self.visits = 0
+        self.positionX = 0
+        self.positionY = 0
+        self.positionZ = 0
         
         
         self.planets = planets
@@ -61,11 +157,26 @@ class PlanetarySystem(object):
 
         for planet in planets:
             self.orbits[planet.orbit] = planet
+            
+    #Get the tech level for a particular planet.
+    def getTechLevel(orbit):
+        self.orbits[orbit].getTechLevel(self.systemName)
         
-def initialisePlanets(filename):
+    #Update the system, which is normally based on time passed since last visit.
+    def updateSystem(self):
+        pass
+
+
+def initialisePlanets(fileName):
     #load planet files and populate planet structure
     #planet by name = (planet name, state, variation, tech level/life)
     Planets["mars"] = ("mars",4,'C',5)
+
+
+#Note: Make sure you initialise the planets!
+def initialiseSystems(fileName="Data_Generators\Other\Ironpy_SystemData.tab")
+
+    pass
     
 def transformCheckPlanet(planet):
     name, state, grade, life = Planets[planet]
@@ -73,4 +184,33 @@ def transformCheckPlanet(planet):
     
     #BIG ALGO HERE
     Planets[planet] = (name,state,grade)
+
+#Render a planet using an approximation of the old IronSeed Algorithm.
+#Note: I'm thinking high-quality pre-renders might be a better choice.
+#landtype= array[1..240,1..120] of byte;
+def renderPlanet(width, height, planetType, surface, step=0):
+    comboSurface = pygame.Surface(g.size)
+    finished = False
+    #comboSurface.set_alpha(step*10)
+    safeSurface = pygame.PixelArray(surface)
+    safeCombo = pygame.PixelArray(comboSurface)
+    #we create the planet first, then blit the pixels onto the original
+    #surface.  Unfortunately, the creation process is not fast.
     
+    
+    
+    # Now to the copy, taking into account the transparency layer.
+    line = 0
+    while line<g.height:
+        for pixel in range(g.width):
+            if safeCombo[pixel][line] != 0:
+                safeSurface[pixel][line]=safeCombo[pixel][line]            
+            
+        line += 1
+            
+    #surface.blit(comboSurface,(0,0))
+    del safeSurface
+    del safeCombo
+    if (step*10) >= 255:
+        finished = True
+    return finished
