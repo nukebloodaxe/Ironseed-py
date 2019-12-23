0c8cf6c4342f3bd2108b0b9ffdb2ca7b2cd1ec31
Nuke Bloodaxe <nuke_bloodaxe@nuke-bloodaxe.org>
2019-12-24 12:36:17 +1300

    Add Item Types
    Adding Item Types to items definition file and adding support for it to loader.

--- items.py
+++ items.py
@@ -9,19 +9,24 @@ import io
 itemDictionary = {}
 itemConstructionDictionary = {}
 
-#Base object parameters.
+# Items are divided into the following types:
+# Weapons, Shields, Devices, Components, Materials, Elements, Artifacts.
+# Artifacts will be implmented later, they are a special case.
+
+# Base object parameters.
 class Item(object):
-    def __init__(self, name, cargoSize, worth, levels):
+    def __init__(self, name, cargoSize, worth, levels, itemType):
         self.name = name
         self.cargoSize = cargoSize
         self.worth = worth
-        self.levels = levels #These appear to be the level requirements
-                             #for each type of crew member. [6 crew, 6 levels.]
-        #[psychometry, engineering, science, security, astrogation, medical]
-        self.description #Item description.
+        self.levels = levels # These appear to be the level requirements
+                             # for each type of crew member. [6 crew, 6 levels.]
+        # [psychometry, engineering, science, security, astrogation, medical]
+        self.description # Item description.
+        self.itemType # The item type.
 
 # This data should be added to a dictionary, by name, on load.
-#By tradition, the Iron Seed three items requirement is used.
+# By tradition, the Iron Seed three items requirement is used.
 class createItem(Item):
     def __init__(self, name, cargoSize, worth, levels, part1, part2, part3):
         self.part1 = part1
@@ -29,25 +34,25 @@ class createItem(Item):
         self.part3 = part3
         Item.__init__(self,name,cargoSize,worth,levels)
 
-#Populate the item and item construction dictionaries.
-#we load from two different data files to do this, tab delimited.
-#Data Order: Name, cargosize, worth, part1, part2, part3, levels
+# Populate the item and item construction dictionaries.
+# we load from two different data files to do this, tab delimited.
+# Data Order: Name, cargosize, worth, part1, part2, part3, levels
 def loadItemData(file1="Data_Generators\Other\IronPy_items.tab",
                  file2="Data_Generators\Other\IronPy_itemdata.tab",
                  file3="Data_Generators\Other\IronPy_iteminfo.tab"):
     itemFile = io.open(file1, "r")
-    itemString = itemFile.readline() #title line
-    itemString = itemFile.readline() #spacer line
-    itemString = itemFile.readline() #real data
+    itemString = itemFile.readline() # title line
+    itemString = itemFile.readline() # spacer line
+    itemString = itemFile.readline() # real data
     constructFile = io.open(file2, "r")
-    constString = constructFile.readline() #title line
-    constString = constructFile.readline() #spacer line
-    constString = constructFile.readline() #real data    
+    constString = constructFile.readline() # title line
+    constString = constructFile.readline() # spacer line
+    constString = constructFile.readline() # real data    
     iteminfoFile = io.open(file3, "r")
     iteminfoString = iteminfoFile.readline() # immediate real data
-    S1 = itemString #used plenty, so must be short, is read string from file.
-    S2 = constString #Used plenty, so must be short, is read string from file.
-    S3 = iteminfoString #Used plenty, so must be short, is read string from file.
+    S1 = itemString # used plenty, so must be short, is read string from file.
+    S2 = constString # Used plenty, so must be short, is read string from file.
+    S3 = iteminfoString # Used plenty, so must be short, is read string from file.
     
     while S2 != "":
         decodedConst = S2.split('\t')
@@ -55,6 +60,7 @@ def loadItemData(file1="Data_Generators\Other\IronPy_items.tab",
         for integer in decodedConst[5:]:
             if integer != '\n':
                 temp.append(int(integer))
+        # Item to create, Part 1, Part 2, Part 3, Worth, Required crew levels.
         itemConstructionDictionary[decodedConst[0]] = [decodedConst[0],
                                                        decodedConst[1],
                                                        decodedConst[2],
@@ -65,28 +71,32 @@ def loadItemData(file1="Data_Generators\Other\IronPy_items.tab",
         
     while S1 != "":
         decodedItem = S1.split('\t')
-        dump = decodedItem[1].split('\n') # removing newline
-        decodedItem[1] = int(dump[0])
+        dump = decodedItem[2].split('\n') # removing newline
+        decodedItem[2] = dump[0]
+        # Name, Cargo Size, Item Type, Item Worth, Required Crew Levels.
         try:
             itemDictionary[decodedItem[0]] = [decodedItem[0],
-                                              decodedItem[1],
+                                              int(decodedItem[1]),
+                                              decodedItem[2],
                                               itemConstructionDictionary[decodedItem[0]][4],
                                               itemConstructionDictionary[decodedItem[0]][5]]
         except KeyError:
-            #We don't care about missing items.
-            #We set these up as singular instances.
+            # We don't care about missing items.
+            # We set these up as singular instances.
             try:
                 itemDictionary[decodedItem[0]] = [decodedItem[0],
                                                   decodedItem[1],
+                                                  decodedItem[2],
                                                   0,[1,1,1,1,1,1]]
             except:
                 print("Tried Key:", decodedItem[0])
-                #Usually indicates the file we are loading is incorrectly
-                #formatted.  If you are modding, double-check your tabs.
+                # Usually indicates the file we are loading is incorrectly
+                # formatted.  If you are modding, double-check your tabs.
                 print("Absolutely fatal error on creating items")    
             
         S1 = itemFile.readline()
     
+    # Add item descriptions.
     while S3 != "ENDF":
         itemName = S3.split('\n')[0]
         S3 = iteminfoFile.readline().split('\n')[0]
@@ -98,9 +108,9 @@ def loadItemData(file1="Data_Generators\Other\IronPy_items.tab",
             itemDictionary[itemName].append(itemDescription)
         except KeyError:
             print("Tried Key:", itemName)
-            #Usually indicates the file we are loading is incorrectly
-            #formatted.  If you are modding, double-check your item
-            #files, as you may have a spelling mistake in the key names.
+            # Usually indicates the file we are loading is incorrectly
+            # formatted.  If you are modding, double-check your item
+            # files, as you may have a spelling mistake in the key names.
             print("Absolutely fatal error on adding item description.")    
         
         # An Item Description has now been loaded.
