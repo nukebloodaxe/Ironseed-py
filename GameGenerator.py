49dcb4c1b6e8194ea97b028d8e3f9c69705b3226
Nuke Bloodaxe <nuke_bloodaxe@nuke-bloodaxe.org>
2019-12-29 18:20:45 +1300

    Preparation for drawing animations + buttons
    This is mainly required for running the ball raise and lower functions.  Other outlines are given for draw functions for crew portraits and the "heartbeat" status line.

--- GameGenerator.py
+++ GameGenerator.py
@@ -11,11 +11,12 @@ are saved to disk as a savegame file.
 """
 
 import io, pygame, crew, ship, items, planets, random, global_constants as g
+import buttons
 
 class Generator(object):
     def __init__(self):
-        self.currentShip = ship.Ship() #Should initialise to default start values.
-        self.systemState = 1 #By default, the game Generator points at itself.
+        self.currentShip = ship.Ship() # Should initialise to default start values.
+        self.systemState = 1 # By default, the game Generator points at itself.
         self.generationStage = 1 # The stage of generation are we at.
         self.portraits = []
         crew.loadCrewData()  # To make things simpler later.
@@ -25,7 +26,21 @@ class Generator(object):
         self.crewSelector = pygame.image.load("Graphics_Assets\\char2.png")
         self.crewSelectorScaled = pygame.transform.scale(self.crewSelector,(g.width,g.height))
         
-        #Generate planetary systems.
+        # define button positions for a 640x480 screen.
+        # Note: expect this to be very buggy!  Placeholder class in effect.
+        
+        
+        # Animations in effect
+        self.raiseBall = False
+        self.raiseBallFrame = 0
+        self.lowerBall = False
+        self.lowerBallFrame = 0
+        
+        self.changePortrait = False
+        self.oldPortrait = 0
+        self.newPortrait = 0
+        
+        # Generate planetary systems.
         planets.loadPlanetarySystems()
         planets.initialisePlanets()
         planets.populatePlanetarySystems()
@@ -38,26 +53,39 @@ class Generator(object):
             else:
                 self.portraits.append(pygame.image.load(file+str(image)+fileType))
     
-    #Draw all crew related surfaces.
+    # Draw a portrait of a crewmember, if the swap animation is in effect we
+    # draw the old portrait being changed to the new one.
+    def drawPortrait(self, portrait):
+        
+        pass
+    
+    # Draw all crew related surfaces.
     def drawCrew(self, displaySurface):
         displaySurface.blit(self.crewSelectorScaled,(0,0))        
         
+    # Draw the status line.
+    def drawStatusLine(self, crewMember, displaySurface):
+        
+        
+        
+        pass
     
-    #Draw all ship related surfaces
+    
+    # Draw all ship related surfaces
     def drawShip(self, displaySurface):
         displaySurface.blit(self.shipCreatorScaled,(0,0))        
         
     
-    #On end, save the data that has been generated to a filename of users choice.
+    # On end, save the data that has been generated to a filename of users choice.
     def saveData(self, fileName="Default"):
         
         pass
     
-    #update function for main game loop.
+    # Update function for main game loop.
     def update(self, displaySurface):
         return self.runGenerator(displaySurface)
     
-    #main generator game loop.
+    # Main generator game loop.
     def runGenerator(self, displaySurface):
         if self.generationStage == 1:
             self.drawShip(displaySurface)
