aebe091da34524d6d23616cea7401cd597460332
Nuke Bloodaxe <nuke_bloodaxe@nuke-bloodaxe.org>
2020-01-10 21:49:58 +1300

    Update alien.py
    Populate value allocations.

--- alien.py
+++ alien.py
@@ -9,6 +9,37 @@ Alien Ships: The aliens live on ships as well, strange that.
 Aliens = {} # All alien factions, can custom populate during game!
 AlienConversations = {} # Alien conversation sets by type.
 
+class AlienShip(object):
+    
+    def __init__(self, relativeX, relativeY, relativeZ, techLevel, skill,
+                 shield, battery, shieldLevel, hullDamage,
+                 destinationX, destinationY, destinationZ, maxHull,
+                 maxAcceleration, regeneration, shipPicture, alien,
+                 name = "Nostromo" ):
+        self.relativeX = relativeX
+        self.relativeY = relativeY
+        self.relativeZ = relativeZ
+        self.techLevel = techLevel
+        self.skill = skill
+        self.shield = shield
+        self.battery = battery
+        self.shieldLevel = shieldLevel
+        self.hullDamage = hullDamage
+        self.destinationX = destinationX
+        self.destinationY = destinationY
+        self.destinationZ = destinationZ
+        self.maxHull = maxHull
+        self.maxAcceleration = maxAcceleration
+        self.regeneration = regeneration
+        self.shipPicture = shipPicture
+        self.alien = alien
+        self.name = name
+        self.damage = [0, 0, 0, 0, 0, 0, 0]
+        self.gunNodes = [0, 0, 0, 0, 0]
+        self.charges = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+
 class Alien(object):
     def __init__(self, name = "FaceHugger", alienType = 0, techRange = (0,0),
                  Anger = 0, congeniality = 0, victory = 0, id = 0,
