6bb79396c93b41773d5fd32adf6885d8d8c13d43
Nuke Bloodaxe <nuke_bloodaxe@nuke-bloodaxe.org>
2020-01-16 20:23:06 +1300

    Bumped up planet size default - Added some more colours
    The planet size was pretty small, and it was making development a bit difficult, so I've double it to something a bit more sensible.  A planet now has a diameter of 240 pixels.
    
    The colours for technology levels have been added, and will likely change in the future.

--- global_constants.py
+++ global_constants.py
@@ -7,27 +7,34 @@ Global Constants, used everywhere.
 import pygame
 
 size = width, height = 640, 480 # screen dimensions
-#Planet texture constants.
-planetHeight = 120
-planetWidth = 240
+#  Planet texture constants.
+planetHeight = 240 # 120
+planetWidth = 480 # 240
 version = "IronPython 0.01 - FrigidSnake Alpha"
 
-# Initialise music system and pygame
+#  Initialise music system and pygame
 pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.init()
 
-#colours
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
+#  Colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 
-#fonts
+TECH1 = (50, 50, 0)
+TECH2 = (100, 100, 0)
+TECH3 = (150, 150, 0)
+TECH4 = (200, 200, 0)
+TECH5 = (250, 250, 0)
+
+#  Fonts:  this is a temporary google font, get it from them.
 font = pygame.font.Font('Fonts\\NotoSansTC-Regular.otf', 12)
 offset = 20 # for this font.
 
-# Totals for items
+#  Totals for items
 totalElements = 17
 totalMaterials = 21
 totalComponents = 22
@@ -38,11 +45,11 @@ maxCargo = totalElements + totalMaterials + totalMaterials + totalComponents
 maxCargo += totalDevices + totalShields + totalWeapons + 1
 
 
-#game related constants; will be class integrated later.
+#  Game related constants; will be class integrated later.
 gameStatus = 0
 
-eventFlags = [] # Having the events system as flags makes things much simpler.
+eventFlags = [] #  Having the events system as flags makes things much simpler.
 
 systemsVisited = []
 
-starDate = [2,3,3784,8,75] #M,D,Y,H,M, Default entry here is for new game.
+starDate = [2, 3, 3784, 8, 75] #M,D,Y,H,M, Default entry here is for new game.
