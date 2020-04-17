e7e4df7c1dc884c56464d58800c06ea9cbd360f8
Nuke Bloodaxe <nuke_bloodaxe@nuke-bloodaxe.org>
2020-04-18 00:29:44 +1200

    Formatting + IronSeed time support + multicolour fade
    Adjusted formatting for readability.  I've added a specific IronSeed date/time system, with supporting formatting functions, for use in the game displays + Date/Time logic.
    The fade function has been extended and now supports fade to different colours, this allows fade to red as is used in the intro and during game over in battle.

--- helper_functions.py
+++ helper_functions.py
@@ -7,11 +7,11 @@ Ironseed helper functions
 import pygame, random, time, math
 import global_constants as g
 
-
 #  Create a colour gradient, from black to the colour in the given length.
 #  The colour is expected to be a Tuple: (0, 0, 0)
 #  The return is a list of tuples; provides max compatibility.
 def colourGradient(length, colour):
+    
     pixels = [(0, 0, 0)]
     step0 = colour[0]/length
     step1 = colour[1]/length
@@ -19,7 +19,9 @@ def colourGradient(length, colour):
     pixel0 = 0
     pixel1 = 0
     pixel2 = 0
+    
     for pixel in range(length):
+        
         pixel0 += step0
         pixel1 += step1
         pixel2 += step2
@@ -31,6 +33,7 @@ def colourGradient(length, colour):
 #  The colour is expected to be a Tuple: (0, 0, 0)
 #  The return is a list of tuples; provides max compatibility.
 def colourLine(length, colour):
+    
     pixels = [colour]
 
     for pixel in range(length):
@@ -46,24 +49,36 @@ def colourLine(length, colour):
 #  half a hemesphere with Height diameter.
 #  Returns a pygame surface with the required elements added.
 def createBar(tupleList=[], length=0, height=int((g.height/320)*2), rounded=False):
+    
     bar = pygame.Surface((length, height))
     bar.set_colorkey(g.BLACK)
     barArray = pygame.PixelArray(bar)
     roundedY = 0
     roundStep = 0
+    
     if rounded:
+        
         roundedY = int(height/2)
         
     
     for x in range(length):
+        
         for y in range(height):
+            
             if rounded:
+                
                 if (length - roundedY) <= x:
+                    
                     if y-(x-(length - roundedY)) > 0 and y < y+(x-(length - roundedY)):
+                        
                         barArray[x][y] = tupleList[x]
+                        
                 else:
+                    
                     barArray[x][y] = tupleList[x]
+                    
             else:
+                
                 barArray[x][y] = tupleList[x]
                 
     barArray.close()
@@ -74,6 +89,7 @@ def createBar(tupleList=[], length=0, height=int((g.height/320)*2), rounded=Fals
 #  BIG NOTE:  Complexity comes from ridiculous math.sqrt not handling negative
 #             numbers...
 def map(x, y, xWidth, yHeight):
+    
     #  x * math.sqrt(1 - y * y / 2), y * math.sqrt(1 - x * x / 2)
     #  Determine in which quadrant circle unit value will appear.
     #  And convert x and y to temporary unit circle values
@@ -86,20 +102,28 @@ def map(x, y, xWidth, yHeight):
     #  top right = 3, bottom right = 2, bottom left = 1, top left = 4.
     
     if x <= radius:
+        
         if y <= radius:
+            
             quadrant = 4 #  top left
             xConverted = -1 * (radius-x) * unitCircleMultiplier#x * unitCircleMultiplier
             yConverted = -1 * (radius-y) * unitCircleMultiplier#y * unitCircleMultiplier
+            
         else:
+            
             quadrant = 3 #  Top right - contains top right data.
             xConverted = -1 * (radius-x) * unitCircleMultiplier
             yConverted = (radius - y) * unitCircleMultiplier
     else:
+        
         if y <= radius:
+            
             quadrant = 1 #  bottom left?
             xConverted = (radius - x) * unitCircleMultiplier
             yConverted = -1 * (radius - y) * unitCircleMultiplier
+            
         else:
+            
             quadrant = 2  #  Appears to be bottom right data
             xConverted = -1*(radius - x) * unitCircleMultiplier
             yConverted = -1*(radius - y) * unitCircleMultiplier
@@ -115,14 +139,21 @@ def map(x, y, xWidth, yHeight):
     yUnit = 0
     
     if tempxUnit < 0:
+        
         xUnit = xConverted * (-1*sqrt(-1*tempxUnit))
+        
     else:
+        
         xUnit = xConverted * sqrt(tempxUnit)
     
     if tempyUnit < 0:
+        
         yUnit = yConverted * (-1*sqrt(-1*tempyUnit))
+        
     else:
+        
         yUnit = yConverted * sqrt(tempyUnit)
+        
     #yUnit = yConverted * math.sqrt(1 - (xConverted * xConverted) / 2)
     
     #print("X Unit: ", str(xUnit), "Y Unit: ", str(yUnit))
@@ -135,7 +166,9 @@ def map(x, y, xWidth, yHeight):
     #  Convert to normal space values.
     xRealSpace = 0
     yRealSpace = 0
+    
     if quadrant == 1:
+        
         #  Bottom Left - confirmed working.
         xRealSpace = int(radius - xMap)
         yRealSpace = int(yMap + radius)
@@ -151,6 +184,7 @@ def map(x, y, xWidth, yHeight):
         yRealSpace = int(radius - yMap)
         
     else:  #  Top Left, confirmed correct.
+    
         xRealSpace = -1 * int(radius-xMap)
         yRealSpace = -1 * int(radius-yMap)
     
@@ -163,17 +197,20 @@ def map(x, y, xWidth, yHeight):
 class StopWatch(object):
 
     def __init__(self):
+        
         self.stopwatch = 0
         self.stopwatchSet = False
         self.time = 0
 
     #  Get the current time - Real World.
     def getTime(self):
+        
         self.time = time.time()
         return self.time
 
     #  Set Stopwatch - Real World
     def setStopwatch(self):
+        
         self.stopwatchSet = True
         self.stopwatch = time.time()
 
@@ -181,74 +218,181 @@ class StopWatch(object):
     #  Should result in current time and later calc of 0 in other calling
     #  functions.
     def getStopwatch(self):
+        
         if self.stopwatchSet is True:
+            
             return self.stopwatch
+        
         return time.time()
 
     #  Get seconds since stopwatch was set.
     def getElapsedStopwatch(self):
+        
         return time.time() - self.stopwatch
     
     #  Reset StopWatch
     def resetStopwatch(self):
+        
         self.stopwatchSet = False
         self.stopwatch = 0
 
+#  Create a global stopwatch, this gets used all over the place.
 GameStopwatch = StopWatch()  #  Very much needed.
 
-# Increment "game" time, this is IronSeed universe time.
+#  Prefix a number with zeros for places width and return it as a string.
+#  If the places figure is 0 or less, return the number as a string.
+#  If the number is greater than the length of the places, return the number.
+def zeroPrefix(self, number, places):
+    
+    if places <=0:
+        
+        return str(number)
+    
+    result = ''
+    
+    if len(str(number)) >= places:
+        
+        return str(number)
+    
+    prefixCount = places - len(str(number))
+    
+    for index in range(1, prefixCount):
+        
+        result += '0'
+    
+    result += str(number)
+    
+    return result
 
+# Implement "game" time, this is IronSeed universe time.
+
+class IronSeedTime(object):
+    
+    def __init__(self):
+
+        #  Set stardate from global value.        
+        starDateYear = g.starDate[0]
+        starDateMonth = g.starDate[1]
+        starDateDay = g.starDate[2]
+        starDateHour = g.starDate[3]
+        starDateMinute = g.starDate[4]
+
+    #  Load new values, usually from Save Game.        
+    def loadNewDate(self, Year, Month, Day, Hour, Minute):
+        
+        starDateYear = Year
+        starDateMonth = Month
+        starDateDay = Day
+        starDateHour = Hour
+        starDateMinute = Minute
+
+    #  Retrieve game time as list, for special displays and save game.
+    def getGameTime(self):
+        
+        return [self.starDateYear, self.starDateMonth, self.starDateDay,
+                self.starDateHour, self.starDateMinute]
+    
+    #  Return string of IronSeedTime, usually for display.
+    #  Note:  For some reason time was in blue on black...
+    def __str__(self):
+        
+        return zeroPrefix(self.starDateDay, 2)+'/'+zeroPrefix(self.starDateMonth, 2)+'/'+str(self.starDateYear)+'  '+zeroPrefix(self.starDateHour, 2)+':'+zeroPrefix(self.starDateMinute, 2)
+    
+    #  Tick a unit of time
+    def incrementTime(self, Year=False, Month=False,
+                      Day=False, Hour=False, Minute=False):
+        
+        if Year:
+            self.starDateYear += 1
+        
+        if Month:
+            self.starDateMonth += 1
+            
+        if Day:
+            self.starDateDay += 1
+            
+        if Hour:
+            self.starDateHour += 1
+            
+        if Minute:
+            self.starDateMinute += 1
 
 
 #  Check event flags to see if the event has been tripped.
 #  Note: negative events and events above 20000 always return true.
 def checkEvent(event):
+    
     if event < 0 or event >= 20000:
+        
         return True
     
     if event >= 8192:
+        
         return False
     
     if event in g.eventFlags:
+        
         return True
+    
     else:
+        
         return False
 
 #  Safe wrapping at a given step and number
 def safeWrap(width, step, current):
+    
     whereAt = current+step
+    
     if whereAt >= width:
+        
         return whereAt % width
+    
     return whereAt
 
 #  Render the given text onto a surface.
 def renderText(text, font, Surface, colour, offset, width=0, height=0, centred=False):
+    
     position = 0
+    
     for line in text:
+        
         renderedText = font.render(line, True, colour)
+        
         if centred:
+            
             Surface.blit(renderedText, (width-(renderedText.get_width()/2), height+position))
+            
         else:
+            
             Surface.blit(renderedText, (width, height+position))
+            
         position += offset
 
 #  Print text at height+width of given surface, fading in.
 def fadeInText(text, width, height, colour, surface, step=0, darken=False, centreText=True):
+    
     comboSurface = pygame.Surface(g.size)
     finished = False
+    
     if darken:
+        
         fade = pygame.Surface(g.size)
         fade.fill(g.BLACK)
         fade.set_alpha(20)
         surface.blit(fade, (0, 0))
+        
     renderText(text, g.font, comboSurface, colour, 20, width, height, True)
     comboSurface.set_alpha(step*10)
     safeSurface = pygame.PixelArray(surface)
     safeCombo = pygame.PixelArray(comboSurface)
     line = 0
+    
     while line < g.height:
+        
         for pixel in range(g.width):
+            
             if safeCombo[pixel][line] != 0:
+                
                 safeSurface[pixel][line] = safeCombo[pixel][line]            
 
         line += 1
@@ -256,37 +400,49 @@ def fadeInText(text, width, height, colour, surface, step=0, darken=False, centr
     #surface.blit(comboSurface,(0,0))
     del safeSurface
     del safeCombo
+    
     if (step*10) >= 255:
+        
         finished = True
+        
     return finished
 
 
 #  Fade out a given surface.
 def fadeOut(width, height, surface, step, fillColour = g.BLACK):
+    
     finished = False
     fade = pygame.Surface(g.size)
-    fade.fill(g.BLACK)
+    fade.fill(fillColour)
     fade.set_alpha(step*5)
     surface.blit(fade, (0, 0))
+    
     if step >= 55:
+        
         finished = True
+        
     return finished
 
 #  Fade in a given surface.
 def fadeIn(width, height, surface, step, fillColour = g.BLACK):
+    
     finished = False
     fade = pygame.Surface(g.size)
     fade.fill(g.BLACK)
     fade.set_alpha(255-step*5)
     surface.blit(fade, (0, 0))
+    
     if step >= 55:
+        
         finished = True
+        
     return finished
 
 #  Take a piece of text and converge it on a central location from 4
 #  different directions, with god-ray effects.
 #  step indicates which step of the transformation to illustrate.
 def convergeText(text, font, offset, colour, width, height, surface, step=0, darken=True):
+    
     ratio = height/width
     ratio2 = width/height
     centre = False
@@ -297,16 +453,21 @@ def convergeText(text, font, offset, colour, width, height, surface, step=0, dar
     y2 = ((height*ratio2)/5)*2.5#/5)*2.5#(height/4)*2#7*3.5
     
     if darken:
+        
         fade = pygame.Surface(g.size)
         fade.fill(g.BLACK)
         fade.set_alpha(10)
         surface.blit(fade, (0,0))
+        
     renderText(text, font,surface, colour, offset, x1+step, y1+step, True)
     renderText(text, font,surface, colour, offset, x2-step, y1+step, True)
     renderText(text, font,surface, colour, offset, x1+step, y2-step, True)
     renderText(text, font,surface, colour, offset, x2-step, y2-step, True)
+    
     if (int(x1+step) - int(x2-step)) <= 1 and (int(x1+step) - int(x2-step)) >= -1:
+        
         centre = True
+        
     return (centre, x1+step, y1+step)
 
 #  Create TV Fuzz - Quickly, with stylish half-fill by default
@@ -320,13 +481,21 @@ def makeFuzz(width, height, half=True):
     C = random.choice
     S = fuzzyScreen.set_at
     yrange = range(height)
+    
     if half is True:
+        
         yrange = range(0, height, 2)
+        
     xrange = range(width)
+    
     for y in yrange:
+        
         #if y%2 == 0 or not half:
         #    for x in xrange:
         #        S((x,y),C(colours))
+        
         for x in xrange:
+            
             S((x, y), C(colours))
+            
     return fuzzyScreen
