fd681b44521803fc03b75d58b3bd3e2a7186f42d
Nuke Bloodaxe <nuke_bloodaxe@nuke-bloodaxe.org>
2020-05-09 16:23:25 +1200

    Outline class and functions for the command deck
    The Command Deck really needs a class now, this will allow for beter testing of the functions classes which have been implemented so far; as in the limited cases of the EGO Synth manipulator and planet scanner.

---
+++ commandDeck.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 13:10:10 2020
+
+Main Command Deck, central operations for all systems in IronSeed.
+
+@author: Nuke Bloodaxe
+"""
+
+import pygame, os
+
+class CommandDeck(object):
+    
+    def __init__(self, ship, crew):
+        
+        self.ironSeed = ship
+        self.crewMembers = crew
+        self.commandStage = 0
+        self.systemState = 10
+        self.musicState = False
+        
+    #  Mouse button interaction routine.
+    def interact(self, mouseButton):
+        
+        currentPosition = pygame.mouse.get_pos()
+        
+        return self.systemState
+    
+    #  Draw command deck interface
+    def drawInterface(self, displaySurface):
+        
+        pass
+    
+    
+    #  Check stage and run routines for initialization, ongoing ops or exit.
+    def runCommandDeck(self, displaySurface):
+        
+        if self.commandStage == 0:
+            
+            #  Start deck music
+            if self.musicState == False:
+                
+                pygame.mixer.music.load(os.path.join('sound', 'SECTOR.OGG'))
+                pygame.mixer.music.play()
+                self.musicState = True
+                self.commandStage += 1
+            
+        elif self.commandStage == 1:
+            
+            self.drawInterface(displaySurface)
+
+            # rewind and start music playing again if track end reached.
+            if not pygame.mixer.music.get_busy():
+
+                pygame.mixer.music.play()
+        
+        
+        return self.systemState #  loop for the moment.
+    
+    
+    def update(self, displaySurface):
+
+        return self.runCommandDeck(displaySurface)
