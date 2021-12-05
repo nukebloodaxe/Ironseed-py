d7dfad5988dca08e2ee74cfbfd96c06ce7b1642b
Nuke Bloodaxe <nuke_bloodaxe@nuke-bloodaxe.org>
2021-12-05 23:09:11 +1300

    Add all major functions, preliminary text version working
    The initialization screen will now display progress, based on percentages.  This is all text based right now, not including the background graphic.  At the moment this is quite usable, and ready for adding extra graphical goodies later.

--- initializationScreen.py
+++ initializationScreen.py
@@ -25,30 +25,137 @@ class loadingBar(object):
         self.sprite = leadGraphic
         self.barColour = barColour
         self.barText = barText
+        self.stringAvailable = False
+        self.barString = ""
+        self.barTextProgress = [h.rotatingLine(0)]
         self.title = title  # We need to see if this is a section title line.
+        self.rotator = 0
+
+    #  Rotate the rotating character.
+    def progressRotator(self):
+
+        # Keep the rotator turning with each update call.
+        self.rotator += 1
+
+        if self.rotator > 8:
+
+            self.rotator = 0
+
+        self.barTextProgress.pop()  # remove rotator character.
+        self.barTextProgress.append(h.rotatingLine(self.rotator))
+
+    # Set the progress bar to a new length, based on a percentage and
+    # character limit.
+    def setProgressBar(self, percentage, limit):
+
+        stars = int(percentage * (limit/100))
+        self.barTextProgress = []
+
+        for char in range(stars):
+
+            self.barTextProgress.append('*')
+
+        if percentage < 100:
+
+            self.barTextProgress.append('|')
+
+        self.stringAvailable = False
+
+    # Get the current progress bar as a string
+    def getProgressBar(self):
+
+        if not self.stringAvailable:
+
+            self.barString = ""
+
+            for char in self.barTextProgress:
+
+                self.barString += char
+
+            self.stringAvailable = True
+
+        return self.barString
 
 
-class initializationScreen(object):
+class initScreen(object):
 
-    def __init__(self):
+    def __init__(self, displaySurface):
 
         self.loadingBars = []  # Progress bars and text for each section.
         self.step = 0  # We need to keep track of our process steps.
         self.barIndex = 0  # Index of first bar drawn on screen.
 
         # Graphics related section
+        self.displaySurface = displaySurface
         self.backgroundScreen = pygame.image.load(os.path.join('Graphics_Assets', 'saver.png'))
+
+        # Scaled Graphics
+        self.scaledBackgroundScreen = pygame.transform.scale(self.backgroundScreen, (g.width, g.height))
+        self.scaledBackgroundScreen.set_colorkey(g.BLACK)
+
+        # Line and character limits for this resolution.
+        self.lineLimit = h.textLinesWithin(0, g.height)
+        self.characterLimit = h.charactersWithin(0, g.width)
+
+    #  Add a new loading bar
+    def addLoadingBar(self, leadGraphic, barColour, barText, title=False):
+
+        self.loadingBars.append(loadingBar(leadGraphic, barColour, barText, title))
+
+    #  Set the process step we are on.
+    def setProcessStep(self, step):
+
+        self.step = step
+
+        # Check to see which step we need to render from, so we don't overshoot
+        # the bottom of the screen.
+        if self.step > self.lineLimit:
+
+            self.barIndex = self.lineLimit - self.step
 
     #  Rendering routine, we use the default screen size.
     def screenRender(self):
 
-        pass
+        self.displaySurface.fill(g.BLACK)
+        self.displaySurface.blit(self.scaledBackgroundScreen, (0, 0))
+
+        # now coalate the text and render it.
+        textToPrint = []
+
+        for bar in self.loadingBars[self.barIndex:]:
+
+            textToPrint.append(bar.barText)
+            textToPrint.append(bar.getProgressBar())
+
+        # Note: Very simple for now.
+        h.renderText(textToPrint, g.font, self.displaySurface, g.WHITE, g.offset)
+        pygame.display.update()
 
     #  Run the intialization routine, including graphics update functions.
-    def processData(self):
+    #  Progress is the % progress return from another object.
+    def processData(self, progress):
 
+        # Set completed bar length to progress.
+        self.loadingBars[self.step].setProgressBar(progress, self.characterLimit)
+
+        if progress < 100:
+
+            self.loadingBars[self.step].progressRotator()
+
+        if progress == 100:
+
+            return False
+
+        else:
+
+            return True  # return true until all finished.
+
+    #  Update loop.
+    def update(self, progress):
+
+        status = self.processData(progress)
         self.screenRender()
-        return True  # return true until all finished.
+        return status
 
 
 #  Loads all initialization screen data from the given file location.
@@ -71,7 +178,7 @@ def loadScreenData(file=os.path.join('Data_Generators', 'Other', 'IronPy_Initial
         barColour = (h.loadLineStripComment(initializationFile).split('\n')[0]).split('\t')  # Data Line
 #        print("BarColour", barColour)
 
-        if barColour[0] == "EOD":
+        if barColour[0] == "EOD":  # Data entry is a section title.
 
             InitData.append(loadingBar(None, None, barText, True))
 
